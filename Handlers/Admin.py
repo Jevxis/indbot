5b824800ac813b8557ddc7971df226776ab8fdd4
Jevxis <134950988+Jevxis@users.noreply.github.com>
2023-06-13 23:58:36 +0500

    Update Admin.py

--- Handlers/Admin.py
+++ Handlers/Admin.py
@@ -14,99 +14,6 @@ async def make_changes_command(message: types.Message):
     await message.delete()
 
 
-"""
-
-
-class FSMAdmin(StatesGroup):
-    photo = State()
-    city = State()
-    street = State()
-    number = State()
-    describe = State()
-    location_x = State()
-    location_y = State()
-
-
-# провеерка на доступ к админке
-# @dp.message_handler(commands=['moderator'], is_chat_admin=True)
-
-
-
-# начало диалога загрузки нового пункта меню
-# @dp.message_handler(commands='Загрузить', state=None)
-async def cm_start(message: types.Message):
-    if message.from_user.id == ID:
-        await FSMAdmin.photo.set()
-        await message.reply('Загрузите фото места')
-
-
-# ловим первый ответ и пишем в словарь
-async def load_photo(message: types.Message, state: FSMContext):
-    if message.from_user.id == ID:
-        async with state.proxy() as data:
-            data['photo'] = message.photo[0].file_id
-        await FSMAdmin.next()
-        await message.reply('Введите город')
-
-
-# Ловим второй ответ
-async def load_city(message: types.Message, state: FSMContext):
-    if message.from_user.id == ID:
-        async with state.proxy() as data:
-            data['city'] = message.text
-        await FSMAdmin.next()
-        await message.reply('Введите улицу')
-
-
-# Ловим третий ответ
-async def load_street(message: types.Message, state: FSMContext):
-    if message.from_user.id == ID:
-        async with state.proxy() as data:
-            data['street'] = message.text
-        await FSMAdmin.next()
-        await message.reply('Введите номер дома')
-
-
-#
-async def load_number(message: types.Message, state: FSMContext):
-    if message.from_user.id == ID:
-        async with state.proxy() as data:
-            data['number'] = int(message.text)
-        await FSMAdmin.next()
-        await message.reply('Введите описание данного места')
-
-
-async def load_describe(message: types.Message, state: FSMContext):
-    if message.from_user.id == ID:
-        async with state.proxy() as data:
-            data['describe'] = message.text
-        await FSMAdmin.next()
-        await bot.send_message(message.from_user.id, "поделитесь геопозицией", reply_markup=client_kb.kb_client1)
-
-
-async def handle_location(message: types.Message, state: FSMContext):
-    lat = message.location.latitude
-    lon = message.location.longitude
-    async with state.proxy() as data:
-        data['location_x'] = lat
-    await FSMAdmin.next()
-    async with state.proxy() as data:
-        data['location_y'] = lon
-    await sqlite_db.sql_add_command(state)
-    await bot.send_message(message.from_user.id, "Данные добавлены")
-    await state.finish()
-
-
-# Выход из состояний
-async def cancel_handler(message: types.Message, state: FSMContext):
-    current_state = await state.get_state()
-    if current_state is None:
-        return
-    await state.finish()
-    await message.reply('Ок')"""
-
-
-# @dp.callback_query_handler(lambda x: x.data and x.data.startswith('del '))
 async def del_callback_run(callback_query: types.CallbackQuery):
     await sqlite_db.sql_delete_command(callback_query.data.replace('del ', ''))
     await callback_query.answer(text="удалено", show_alert=True)
@@ -124,7 +31,6 @@ async def delete_item(message: types.Message):
                                                             callback_data=f'del {ret[-1]}')))
 
 
-# так как у нас все по разным файлам, то нужно зарегестрировать хендлеры
 def register_handlers_admin(dp: Dispatcher):
     dp.register_message_handler(make_changes_command, commands=['moderator'], is_chat_admin=True)
     dp.register_message_handler(delete_item, commands='Удалить')
